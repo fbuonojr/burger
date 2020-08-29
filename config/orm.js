e5a1de06cbf53e883c7a1e7f6d367ef9bd2f3a31
Frank Buono <fbuonojr@fordham.edu>
2020-08-29 14:44:23 -0400

    added updateOne() and module.exports

--- config/orm.js
+++ config/orm.js
@@ -12,10 +12,28 @@ function printQuestionMarks(num) {
     return arr.toString();
 }
 
+function objToSql(ob) {
+    var arr = [];
+
+    for(var key in ob) {
+        var value = ob[key];
+
+        if(Object.hasOwnProperty.call(ob, key)) {
+            if(typeof value === "string" && value.indexOf(" ") >= 0){
+                values = "'" + value + "'";
+            }
+        }
+
+        arr.push(key + "=" + value);
+    }
+
+    return arr.toString();
+}
+
 var orm = {
     selectAll: function (tableInput, cb) {
         var queryString = "SELECT * FROM " + tableInput + ";";
-        connection.query(queryString, function (err, res) {
+        connection.query(queryString, function (err, result) {
             if (err) {
                 throw err;
             }
@@ -34,12 +52,32 @@ var orm = {
 
         console.log(queryString);
 
-        connection.query(queryString, vals, function(err, res) {
+        connection.query(queryString, vals, function(err, result) {
             if(err) {
                 throw err;
             }
 
             cb(result);
         })
+    },
+    updateOne: function(table, objColVals, condition, cb) {
+        var queryString = "UPDATE " + table;
+
+        queryString += " SET ";
+        queryString += objToSQL(objColVals);
+        queryString += " WHERE ";
+        queryString += condition;
+
+        console.log(queryString);
+
+        connection.query(queryString, function(err, result) {
+            if(err) {
+                throw err;
+            }
+
+            cb(result);
+        });
     }
-}
+};
+
+module.exports = orm;
