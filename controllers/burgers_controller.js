0d619542a95cabcb44540decef991847e6e44154
Frank Buono <fbuonojr@fordham.edu>
2020-08-29 17:57:56 -0400

    fixed routes

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -14,7 +14,7 @@ router.get("/", function(req, res) {
     });
 });
 
-router.post("/api/cats", function(req, res) {
+router.post("/api/burgers", function(req, res) {
     burger.insertOne([
         "burger_name", "devoured"
     ], [
@@ -24,7 +24,7 @@ router.post("/api/cats", function(req, res) {
     });
 });
 
-router.put("api/cats/:id", function(req, res) {
+router.put("api/burgers/:id", function(req, res) {
     var condition = "id = " + req.params.id;
 
     console.log("condition", condition);
