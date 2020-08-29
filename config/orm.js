931e6f5455e544ce0849c05dd1e218d1718d8e11
Frank Buono <fbuonojr@fordham.edu>
2020-08-29 14:37:02 -0400

    added insertone()

--- config/orm.js
+++ config/orm.js
@@ -11,6 +11,7 @@ function printQuestionMarks(num) {
 
     return arr.toString();
 }
+
 var orm = {
     selectAll: function (tableInput, cb) {
         var queryString = "SELECT * FROM " + tableInput + ";";
@@ -20,5 +21,25 @@ var orm = {
             }
             cb(result);
         });
+    },
+    insertOne: function(table, cols, vals, cb) {
+        var queryString = "INSERT INTO " + table;
+
+        queryString += " (";
+        queryString += cols.toString();
+        queryString += ") ";
+        queryString += "VALUES (";
+        queryString += printQuestionMarks(vals.length);
+        queryString += ") ";
+
+        console.log(queryString);
+
+        connection.query(queryString, vals, function(err, res) {
+            if(err) {
+                throw err;
+            }
+
+            cb(result);
+        })
     }
 }
