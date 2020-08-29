eabc1f974d754e236b091d553aa8764bfc7eec59
Frank Buono <fbuonojr@fordham.edu>
2020-08-29 16:08:12 -0400

    added .get and .post in controller

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,6 +1,27 @@
 var express = require("express");
+
 var burger = require("../models/burger.js");
 
 var router = express.Router();
+
+router.get("/", function(req, res) {
+    burger.selectAll(function(data) {
+        var hbsObject = {
+            burgers: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
+});
+
+router.post("/api/cats", function(req, res) {
+    burger.insertOne([
+        "burger_name", "devoured"
+    ], [
+        req.body.burger_name, req.body.devoured
+    ], function(result) {
+        res.json({ id: result.insertId });
+    });
+});
 
 module.exports = router;
